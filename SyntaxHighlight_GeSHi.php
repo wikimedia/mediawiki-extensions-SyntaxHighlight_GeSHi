60e453d99a6f1f0e9698dd1d0a4a86e1a2b99acb
Chad Horohoe <demon@users.mediawiki.org>
2009-08-05 22:33:30 +0000

    Update last checked version. Also, the ExtensionDistributor bundles the geshi directory, so make a note in README so people don't download it a 2nd time :)

--- SyntaxHighlight_GeSHi.php
+++ SyntaxHighlight_GeSHi.php
@@ -34,8 +34,8 @@
  * If you forget, or give an unsupported value, the extension spits out
  * some help text and a list of all supported languages.
  *
- * The extension has been tested with GeSHi 1.0.8 and MediaWiki 1.14a
- * as of 2008-09-28.
+ * The extension has been tested with GeSHi 1.0.8.3 and MediaWiki 1.15.1
+ * as of 2009-08-05
  */
 
 if( !defined( 'MEDIAWIKI' ) )
