169e22e85617a32a40837d887bc4ef0f7c0a2a76
Chad Horohoe <demon@users.mediawiki.org>
2009-09-04 22:22:12 +0000

    Nuking fallback code for MW < 1.12 in most extensions (there's like 5 or 6 left I haven't done). Couple of points: * 1.11 and below aren't supported anymore, so we don't need to be maintaining back-compat code for it anymore. This is why we branch extensions * The vast majority of these were using $wgParser. This defeats the purpose of ParserFirstCallInit...allowing you to use parsers other than $wgParser. All these extensions now work in any instance of the Parser, not just $wgParser

--- SyntaxHighlight_GeSHi.php
+++ SyntaxHighlight_GeSHi.php
@@ -56,11 +56,7 @@ $wgExtensionMessagesFiles['SyntaxHighlight_GeSHi'] = $dir . 'SyntaxHighlight_GeS
 $wgAutoloadClasses['SyntaxHighlight_GeSHi'] = $dir . 'SyntaxHighlight_GeSHi.class.php';
 $wgHooks['ShowRawCssJs'][] = 'SyntaxHighlight_GeSHi::viewHook';
 $wgHooks['SpecialVersionExtensionTypes'][] = 'SyntaxHighlight_GeSHi::hSpecialVersion_GeSHi';
-if ( defined( 'MW_SUPPORTS_PARSERFIRSTCALLINIT' ) ) {
-	$wgHooks['ParserFirstCallInit'][] = 'efSyntaxHighlight_GeSHiSetup';
-} else {
-	$wgExtensionFunctions[] = 'efSyntaxHighlight_GeSHiSetup_legacy';
-}
+$wgHooks['ParserFirstCallInit'][] = 'efSyntaxHighlight_GeSHiSetup';
 
 /**
  * Register parser hook
@@ -69,10 +65,4 @@ function efSyntaxHighlight_GeSHiSetup( &$parser ) {
 	$parser->setHook( 'source', array( 'SyntaxHighlight_GeSHi', 'parserHook' ) );
 	$parser->setHook( 'syntaxhighlight', array( 'SyntaxHighlight_GeSHi', 'parserHook' ) );
 	return true;
-}
-
-/* Provided for pre-1.12 MediaWiki compatibility. */
-function efSyntaxHighlight_GeSHiSetup_legacy() {
-	global $wgParser;
-	return efSyntaxHighlight_GeSHiSetup( $wgParser );
 }
