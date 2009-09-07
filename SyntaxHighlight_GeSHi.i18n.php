597e23b34b5bad45fffe4e54dd4135a511d72507
Siebrand Mazeland <siebrand@users.mediawiki.org>
2009-09-07 19:20:37 +0000

    Localisation updates for extension messages from translatewiki.net (2009-09-07 18:34 UTC)

--- SyntaxHighlight_GeSHi.i18n.php
+++ SyntaxHighlight_GeSHi.i18n.php
@@ -484,7 +484,7 @@ $messages['ja'] = array(
 	'syntaxhighlight-desc' => '[http://qbnz.com/highlighter/ GeSHi 構文ハイライト機能] を利用したタグ <tt><nowiki><source></nowiki></tt> を提供する',
 	'syntaxhighlight-specify' => '以下のように言語を指定する必要があります:',
 	'syntaxhighlight-supported' => '構文ハイライト機能に対応している言語は以下の通りです:',
-	'syntaxhighlight-err-loading' => '（対応言語一覧の読み込み時にエラーが発生しました）',
+	'syntaxhighlight-err-loading' => '(対応言語一覧の読み込み時にエラーが発生しました)',
 	'syntaxhighlight-err-language' => '不正な言語名です。',
 	'geshi.css' => '/* ここへ、GeShi 構文ハイライト機能に適用する CSS を記述してください */',
 );
