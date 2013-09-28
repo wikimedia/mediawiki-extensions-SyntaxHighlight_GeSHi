bdf76a5c8a54857dc4fcb4af8ad4da829daf5c30
Derk-Jan Hartman <hartman.wiki@gmail.com>
2013-09-28 15:16:27 -0700

    Add extension.json, empty PHP entry point
    While cherry-picking, SyntaxHighlight_GeSHi.langs.php had a conflict
    so I re-ran the update script.
    
    Bug: T88063
    Change-Id: I42afa0bf81f4f89b81a11bcafcb8e4c032a2d173

--- maintenance/updateLanguageList.php
+++ maintenance/updateLanguageList.php
@@ -41,7 +41,10 @@ class UpdateLanguageList extends Maintenance {
 		$langs = array_values( array_filter( $geshi->get_supported_languages( false ), 'ctype_alnum' ) );
 		sort( $langs );
 		$replace = array( '[' => "array(\n\t", ']' => "\n);\n", '",' => "\",\n\t" );
-		$code = "<?php\n" . $header . '$wgGeSHiSupportedLanguages = ' . strtr( json_encode( $langs ), $replace );
+		$code = "<?php\n" .
+			$header .
+			"global \$wgGeSHiSupportedLanguages;\n" .
+			'$wgGeSHiSupportedLanguages = ' . strtr( json_encode( $langs ), $replace );
 		file_put_contents( __DIR__ . '/../SyntaxHighlight_GeSHi.langs.php', $code );
 		$this->output( "Updated language list written to SyntaxHighlight_GeSHi.langs.php\n" );
 	}
