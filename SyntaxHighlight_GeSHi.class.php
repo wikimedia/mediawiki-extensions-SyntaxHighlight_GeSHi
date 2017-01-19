14873aadb3f38efcad1ba07bc4fd422d07328cb8
Edward Chernenko <edwardspec@gmail.com>
2017-01-19 16:14:12 +0300

    Workaround for intermittent problems in Symfony/Process under HHVM.
    Due to https://github.com/facebook/hhvm/issues/6921, $process->getOutput()
    from Symfony/Process doesn't work reliably under HHVM.
    This causes the result of "pygmentize" to be unexpectably truncated.
    
    Until this is fixed in HHVM or Symfony, we can use callback of $process->run()
    instead of the buggy $process->getOutput().
    
    Bug: T151523
    Change-Id: I21ef340eccca0741d7e86840f444272623675c46
    (cherry picked from commit 9debf59c9c68da8856f0a9e529c0b400baf4543c)

--- SyntaxHighlight_GeSHi.class.php
+++ SyntaxHighlight_GeSHi.class.php
@@ -289,7 +289,15 @@ class SyntaxHighlight_GeSHi {
 				->getProcess();
 
 			$process->setInput( $code );
-			$process->run();
+
+			/* Workaround for T151523 (buggy $process->getOutput()).
+				If/when this issue is fixed in HHVM or Symfony,
+				replace this with "$process->run(); $output = $process->getOutput();"
+			*/
+			$output = '';
+			$process->run( function( $type, $capturedOutput ) use ( &$output ) {
+				$output .= $capturedOutput;
+			} );
 
 			if ( !$process->isSuccessful() ) {
 				$status->warning( 'syntaxhighlight-error-pygments-invocation-failure' );
@@ -298,7 +306,6 @@ class SyntaxHighlight_GeSHi {
 				return $status;
 			}
 
-			$output = $process->getOutput();
 			$cache->set( $cacheKey, $output );
 		}
 
