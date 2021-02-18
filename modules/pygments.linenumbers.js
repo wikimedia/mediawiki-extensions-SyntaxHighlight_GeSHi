7d9c268ffd09dcc290a116dd11bd3aff73c853f0
Timo Tijhof <krinklemail@gmail.com>
2021-02-18 05:48:45 +0000

    pygments.linenumbers: Avoid deep jQuery loops on unrelated interactions
    Follows-up 42c97fa (I82ed4ade), 63c5943a9 (Ic71346196).
    
    This commit makes no functional changes, but it reduces overhead
    from the SyntaxHighlight code when interacting with other features,
    such as elaborate modals or other hashchange-heavy code paths that
    a gadget might produce.
    
    It uses vanilla DOM instead of jQuery where possible, and adds an
    early return rather than fairly deep continuation no-ops.
    
    Change-Id: I2264f6f398193802f05c738bad4c294da007fb27

--- modules/pygments.linenumbers.js
+++ modules/pygments.linenumbers.js
@@ -1,30 +1,29 @@
 $( function () {
 
-	var $lastLine;
+	var lastLine;
 
 	function onHashChange() {
 		var id = location.hash.slice( 1 ),
-			// Don't assume location.hash will be parseable as an ID (T271572)
+			// Don't assume location.hash will be parseable as a selector (T271572)
 			// and avoid warning when id is empty (T272844)
-			$line = id ? $( document.getElementById( id ) || [] ) : $( [] );
+			line = id ? document.getElementById( id ) : null;
 
-		if ( !$line.closest( '.mw-highlight' ).length ) {
+		if ( lastLine ) {
+			lastLine.classList.remove( 'hll' );
+		}
+
+		// Support IE 11, Edge 14, Safari 7: Can't use unprefixed Element.matches('… *') yet.
+		if ( !line || !$( line ).closest( '.mw-highlight' ).length ) {
 			// Matched ID wasn't in a highlight block
-			$line = $( [] );
+			lastLine = null;
+			return;
 		}
 
-		if ( $lastLine ) {
-			$lastLine.removeClass( 'hll' );
-		}
-
-		if ( $line.length ) {
-			$line.addClass( 'hll' );
-		}
-
-		$lastLine = $line;
+		line.classList.add( 'hll' );
+		lastLine = line;
 	}
 
-	$( window ).on( 'hashchange', onHashChange );
+	window.addEventListener( 'hashchange', onHashChange );
 
 	// Check hash on load
 	onHashChange();
