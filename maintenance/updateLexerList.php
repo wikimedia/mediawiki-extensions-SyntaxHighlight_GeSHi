57b3580811284ef1acb0667ab015233148052458
Ryan Schmidt <skizzerz@skizzerz.net>
2022-11-20 22:46:55 -0700

    Fix lexer list parsing on Windows
    When using a non-bundled Pygments (which is required on Windows, as the
    bundled version is an ELF binary), we call into the Pygments executable
    to generate the list of supported languages (lexers). This list seems to
    occasionally include carraige returns, causing some languages to not be
    processed correctly. Trim those CRs out so the language list is
    accurate.
    
    Bug: T336087
    Change-Id: If8b1f145dd10e2c4707d6d32927e85d1d2459f15
    (cherry picked from commit 2ae82c7fb7d5b230a354c9db0b07220053f6c0c3)

--- maintenance/updateLexerList.php
+++ maintenance/updateLexerList.php
@@ -56,7 +56,7 @@ class UpdateLexerList extends Maintenance {
 		$output = $result->getStdout();
 		foreach ( explode( "\n", $output ) as $line ) {
 			if ( substr( $line, 0, 1 ) === '*' ) {
-				$newLexers = explode( ', ', trim( $line, "* :\n" ) );
+				$newLexers = explode( ', ', trim( $line, "* :\r\n" ) );
 				// Skip internal, unnamed lexers
 				if ( $newLexers[0] !== '' ) {
 					$lexers = array_merge( $lexers, $newLexers );
